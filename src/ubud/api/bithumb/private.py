1313173471490acfe660ea3b50a638f4c393e5f9
Woojin Cho <woojin.cho@gmail.com>
2022-06-19 13:05:29 +0000

    misc

---
+++ src/ubud/api/bithumb/private.py
@@ -0,0 +1,43 @@
+
+# 회원정보
+"info/account"
+
+# 보유자산
+"info/balance"
+
+# 입금지갑주소
+"info/wallet_address"
+
+# 최근거래정보
+"info/ticker"
+
+# 거래주문내역
+"info/orders"
+
+# 거래주문내역상세
+"info/order_detail"
+
+# 거래체결내역
+"info/user_transaction"
+
+# 지정가주문
+"trade/place"
+
+# 시장가매수
+"trade/market_buy"
+
+# 시장가 매도
+"trade/market_sell"
+
+# 자동주문
+"trade/stop_limit"
+
+# 주문취소
+"trade/cancel"
+
+# 코인출금
+"trade/btc_withdrawal"
+
+
+
+
