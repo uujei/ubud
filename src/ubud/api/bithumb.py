06522e339e9e94ac314e7daf83f44d35967f9946
Woojin Cho <woojin.cho@gmeail.com>
2022-06-26 06:10:43 +0000

    API added

--- src/ubud/api/bithumb.py
+++ src/ubud/api/bithumb.py
@@ -1,6 +1,7 @@
 import base64
 import hashlib
 import hmac
+from typing import List, Callable
 import time
 from urllib.parse import urlencode, urljoin
 import requests
@@ -9,63 +10,120 @@ import logging
 logger = logging.getLogger(__name__)
 
 PATH_PARAMS_PUBLIC = ["order_currency", "payment_currency"]
+DELIM_PATH_PARAMS = "_"
+REQUEST_LIMIT_PREFIX = "X-RateLimit-"
+
+################################################################
+# Handlers
+################################################################
+def log_request_limit(resp):
+    _msg = {k: v for k, v in resp.headers.items() if k.startswith(REQUEST_LIMIT_PREFIX)}
+    logger.info(_msg)
+    print(_msg)
 
 
+################################################################
+# BithumbApi
+################################################################
 class BithumbApi:
+
+    # BITHUMB API URL
+    baseUrl = "https://api.bithumb.com"
+
+    def __init__(
+        self,
+        apiKey: str = None,
+        apiSecret: str = None,
+        apiVersion: str = "unknown",
+        handlers: list = None,
+    ):
+        self.apiKey = apiKey
+        self.apiSecret = apiSecret
+        self.apiVersion = apiVersion
+        self._private_ready = all([c is not None for c in [apiKey, apiSecret]])
+
+        handlers = handlers if handlers is not None else []
+        handlers = handlers if isinstance(handlers, list) else [handlers]
+        self.handlers = handlers + [self._default_handler]
+
+    def _gen_header(self, route, **kwargs):
+        # no required headers for public endpoints
+        if route.strip("/").startswith("public"):
+            return
+
+        # for non-public endpoints
+        api_key = self.apiKey.encode("utf-8")
+        api_secret = self.apiSecret.encode("utf-8")
+        nonce = self._gen_api_nonce()
+        return {
+            "Api-Key": api_key,
+            "Api-Sign": self._gen_api_sign(
+                route=route,
+                nonce=nonce,
+                apiKey=api_key,
+                apiSecret=api_secret,
+                **kwargs,
+            ),
+            "Api-Nonce": nonce,
+        }
+
+    def _gen_request_args(self, route, **kwargs):
+        # for public endpoints
+        if route.strip("/").startswith("public"):
+            path = DELIM_PATH_PARAMS.join([kwargs.pop(p) for p in PATH_PARAMS_PUBLIC if p in kwargs.keys()])
+            url = urljoin(self.baseUrl, route, path)
+            queries = []
+            if "limit" in kwargs.keys():
+                queries += f"counts={kwargs['limit']}"
+            if "chart_interval" in kwargs.keys():
+                queries += kwargs["chart_interval"]
+            if len(queries) > 0:
+                queries = "&".join(queries)
+                url = f"{url}?{queries}"
+            return {
+                "url": url,
+                "headers": None,
+                "data": None,
+            }
+
+        # for non public endpoints
+        kwargs.update({"endpoint": route})
+        headers = self._gen_header(route=route, **kwargs)
+        return {
+            "url": urljoin(self.baseUrl, route),
+            "headers": headers,
+            "data": kwargs,
+        }
+
+    def _request(self, method, route, **kwargs):
+        # get request args
+        args = self._gen_request_args(route, **kwargs)
+
+        # request and get response
+        resp = requests.request(method=method, **args)
+
+        # check status
+        resp.raise_for_status()
+
+        # handler
+        for handler in self.handlers:
+            r = handler(resp)
+            if r is not None:
+                return r
+
     @staticmethod
     def _gen_api_nonce():
         return str(int(time.time() * 1000))
 
     @staticmethod
-    def _gen_api_sign(route, nonce, apiSecret, **kwargs):
+    def _gen_api_sign(route, nonce, apiKey, apiSecret, **kwargs):
         q = chr(0).join([route, urlencode(kwargs), nonce])
         h = hmac.new(apiSecret, q.encode("utf-8"), hashlib.sha512)
         return base64.b64encode(h.hexdigest().encode("utf-8"))
 
     @staticmethod
-    def _validate_and_parse(resp):
-        resp.raise_for_status()
+    def _default_handler(resp):
         body = resp.json()
         if body["status"] != "0000":
             raise ReferenceError(body)
         return body["data"]
-
-
-class BithumbPublic(BithumbApi):
-    def __init__(self):
-        self.baseUrl = "https://api.bithumb.com/public"
-
-    def get(self, route, **kwargs):
-        DELIM = "_"
-        path = DELIM.join([kwargs.pop(p) for p in PATH_PARAMS_PUBLIC if p in kwargs.keys()])
-        url = urljoin(self.baseUrl, route, path)
-        query = ""
-        if "limit" in kwargs.keys():
-            query += f"counte={kwargs['limit']}"
-        if "chart_interval" in kwargs.keys():
-            query += kwargs["chart_interval"]
-        if query != "":
-            url = "?".join([url, query])
-
-        resp = requests.get(url=url)
-        return self._validate_and_parse(resp)
-
-
-class BithumbPrivate(BithumbApi):
-    def __init__(self, apiKey, apiSecret):
-        self.baseUrl = "https://api.bithumb.com"
-        self.apiKey = apiKey.encode("utf-8")
-        self.apiSecret = apiSecret.encode("utf-8")
-
-    def post(self, route, **kwargs):
-        url = urljoin(self.baseUrl, route)
-        kwargs.update({"endpoint": route})
-        nonce = self._gen_api_nonce()
-        headers = {
-            "Api-Key": self.apiKey,
-            "Api-Sign": self._gen_api_sign(route, nonce, self.apiSecret, **kwargs),
-            "Api-Nonce": nonce,
-        }
-
-        resp = requests.post(url=url, headers=headers, data=kwargs)
-        return self._validate_and_parse(resp)
--- src/ubud/api/upbit.py
+++ src/ubud/api/upbit.py
@@ -1,73 +1,107 @@
+import parse
 import base64
+from typing import List, Callable
 import hashlib
 import hmac
-import time
-from urllib.parse import urlencode, urljoin
-import requests
 import logging
+import time
 import uuid
+from urllib.parse import urlencode, urljoin
+
+import jwt
+import requests
 
 logger = logging.getLogger(__name__)
 
-PATH_PARAMS_PUBLIC = ["order_currency", "payment_currency"]
+REMAINING_REQ_FORM = parse.compile("group={group}; min={min}; sec={sec}")
+
+################################################################
+# Hanlders
+################################################################
+def log_request_limit(resp):
+    _remaining_req = resp.headers["Remaining-Req"]
+    _msg = REMAINING_REQ_FORM.parse(_remaining_req).named
+    logger.info(_msg)
+    print(_msg)
 
 
+################################################################
+# UpbitApi
+################################################################
 class UpbitApi:
+
+    # Upbit URL
+    baseUrl = "https://api.upbit.com"
+
+    def __init__(
+        self,
+        apiKey: str = None,
+        apiSecret: str = None,
+        apiVersion: str = "v1",
+        handlers: list = None,
+    ):
+        self.apiKey = apiKey
+        self.apiSecret = apiSecret
+        self.apiVersion = apiVersion
+
+        handlers = handlers if handlers is not None else []
+        handlers = handlers if isinstance(handlers, list) else [handlers]
+        self.handlers = handlers + [self._default_handler]
+
+    def _gen_header(self, route, **kwargs):
+        api_key = self.apiKey
+        api_secret = self.apiSecret
+        nonce = self._gen_api_nonce()
+        return {
+            "Authorization": self._gen_api_sign(
+                route=route,
+                nonce=nonce,
+                apiKey=api_key,
+                apiSecret=api_secret,
+                **kwargs,
+            )
+        }
+
+    def _gen_request_args(self, route, **kwargs):
+        if not route.strip("/").startswith(self.apiVersion):
+            route = "/".join([self.apiVersion, route.strip("/")])
+        headers = self._gen_header(route=route, **kwargs)
+        return {
+            "url": urljoin(self.baseUrl, route),
+            "headers": headers,
+            "data": kwargs,
+        }
+
+    def _request(self, method, route, **kwargs):
+        # get request args
+        args = self._gen_request_args(route, **kwargs)
+
+        # request and get response
+        resp = requests.request(method=method, **args)
+
+        # check status
+        resp.raise_for_status()
+
+        # handler
+        for handler in self.handlers:
+            r = handler(resp)
+            if r is not None:
+                return r
+
     @staticmethod
     def _gen_api_nonce():
         return str(uuid.uuid4())
 
     @staticmethod
-    def _gen_api_sign(route, nonce, apiSecret, **kwargs):
-        q = chr(0).join([route, urlencode(kwargs), nonce])
-        h = hmac.new(apiSecret, q.encode("utf-8"), hashlib.sha512)
-        return base64.b64encode(h.hexdigest().encode("utf-8"))
+    def _gen_api_sign(route, nonce, apiKey, apiSecret, **kwargs):
+        payload = {"access_key": apiKey, "nonce": nonce}
+        if kwargs:
+            m = hashlib.sha512()
+            m.update(urlencode(kwargs).encode())
+            query_hash = m.hexdigest()
+            payload.update({"query_hash": query_hash, "query_hash_alg": "SHA512"})
+        return f"Bearer {jwt.encode(payload, apiSecret)}"
 
     @staticmethod
-    def _validate_and_parse(resp):
-        resp.raise_for_status()
-        body = resp.json()
-        return body["data"]
-
-
-class UpbitQuotation(UpbitApi):
-    def __init__(self):
-        self.baseUrl = "https://api.upbit.com/v1"
-
-    def get(self, route, **kwargs):
-        DELIM = "_"
-        path = DELIM.join([kwargs.pop(p) for p in PATH_PARAMS_PUBLIC if p in kwargs.keys()])
-        url = urljoin(self.baseUrl, route, path)
-        query = ""
-        if "limit" in kwargs.keys():
-            query += f"counte={kwargs['limit']}"
-        if "chart_interval" in kwargs.keys():
-            query += kwargs["chart_interval"]
-        if query != "":
-            url = "?".join([url, query])
-
-        resp = requests.get(url=url)
-        return self._validate_and_parse(resp)
-
-
-class UpbitExchange(UpbitApi):
-    def __init__(self, apiKey, apiSecret):
-        self.baseUrl = "https://api.upbit.com/v1"
-        self.apiKey = apiKey.encode("utf-8")
-        self.apiSecret = apiSecret.encode("utf-8")
-
-    def get(self, route, **kwargs):
-        url = urljoin(self.baseUrl, route)
-
-    def post(self, route, **kwargs):
-        url = urljoin(self.baseUrl, route)
-        kwargs.update({"endpoint": route})
-        nonce = self._gen_api_nonce()
-        headers = {
-            "Api-Key": self.apiKey,
-            "Api-Sign": self._gen_api_sign(route, nonce, self.apiSecret, **kwargs),
-            "Api-Nonce": nonce,
-        }
-
-        resp = requests.post(url=url, headers=headers, data=kwargs)
-        return self._validate_and_parse(resp)
+    def _default_handler(resp, handlers=None):
+        return resp.json()
